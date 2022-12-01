530cac45d5f4ab88a35174dd1cf59a5a72359730
Shakil53 <shakilahmed.dcc@gmail.com>
2022-12-01 13:32:50 +0600

    all most done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
+      <CountriesApi></CountriesApi>
+      <Country></Country>
       
     </div>
   );
+}
+
+function CountriesApi() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then(res => res.json())
+      .then(data => setCountries(data));
+  },[]) 
+  
+  return (
+    
+    <div>
+      <h1>All Countries Around The World</h1>
+      <h4>Countries:{countries.length}</h4>
+      {
+        // countries.map(country => console.log(country.name.common))
+       
+        countries.man(country => <Country name={country.name.common}
+        population={country.name.population}></Country>)
+      }
+    </div>
+  )
+}
+
+function Country(props) {
+  return (
+    <div>
+      <h2>Name: {props.name}</h2> 
+      <h4>population: {props.population}</h4>
+    </div>
+  )
 }
 
 export default App;
